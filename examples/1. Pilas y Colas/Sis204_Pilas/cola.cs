e5f44cd4df797664cc36c00f3b518ab75ea06d99
Grover101 <limachigrover09@gmail.com>
2023-05-10 22:12:04 -0400

    add queues and circular queues

---
+++ examples/1. Pilas y Colas/Sis204_Pilas/cola.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sis204_Pilas
+{
+    class cola
+    {
+        static int maxcola = 3;
+        public int primero;
+        public int ultimo;
+        public int[] lista;
+
+        public cola()
+        {
+            primero = 0;
+            ultimo = -1;
+            lista = new int[maxcola];
+        }
+
+        public bool vacia()
+        {
+            if (ultimo < primero)
+                return true;
+            else
+                return false;
+        }
+
+        public bool llena()
+        {
+            if (ultimo == maxcola - 1)
+                return true;
+            else
+                return false;
+        }
+
+        public void insertar(int x)
+        {
+            if(llena())
+                MessageBox.Show("La cola esta llena, no se puede insertar mas elementos");
+            else
+            {
+                ultimo++;
+                lista[ultimo] = x;
+            }
+        }
+
+        // 0   1  2 3
+        // 45 56 67 67
+        // eliminado: 23
+        public void eliminar()
+        {
+            if(vacia())
+                MessageBox.Show("La cola esta vacia, no se puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("Elemento eliminado es: "+lista[primero]);
+                for(int i=0; i<= ultimo-1; i++)
+                {
+                    lista[i] = lista[i + 1];
+                }
+                ultimo--;
+            }
+        }
+    }
+}
---
+++ examples/1. Pilas y Colas/Sis204_Pilas/colaCircular.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sis204_Pilas
+{
+    class colaCircular
+    {
+        public int maxcola = 3;
+        public int primero;
+        public int ultimo;
+        public String[] lista;
+
+        public colaCircular()
+        {
+            primero = -1;
+            ultimo = -1;
+            lista = new String[maxcola];
+        }
+
+        private bool vacia()
+        {
+            if (primero == -1 && ultimo == -1)
+                return true;
+            else
+                return false;
+        }
+
+        private bool llena()
+        {
+            if ((ultimo == maxcola - 1 && primero == 0) || (ultimo + 1 == primero))
+                return true;
+            else
+                return false;
+        }
+
+        public void insertar(String x)
+        {
+            if(llena())
+                MessageBox.Show("La cola circular esta llena, no se puede insertar mas elementos");
+            else
+            {
+                if (ultimo == maxcola - 1)
+                    ultimo = 0;
+                else
+                    ultimo++;
+                lista[ultimo] = x;
+                if (primero == -1)
+                    primero = 0;
+            }
+        }
+
+        public void eliminar()
+        {
+            if(vacia())
+                MessageBox.Show("La cola circular esta vacio, no se puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("Elemento eliminado es: "+lista[primero]);
+                lista[primero] = "";
+                if(primero == ultimo)
+                {
+                    primero = -1;
+                    ultimo = -1;
+                }
+                else
+                {
+                    if (primero == maxcola - 1)
+                        primero = 0;
+                    else
+                        primero++;
+                }
+            }
+        }
+    }
+}
