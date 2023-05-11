2255c756e87db18a896a5f51549e02f3c3ed873c
Grover101 <limachigrover09@gmail.com>
2023-05-10 22:12:26 -0400

    add  class stack

---
+++ examples/1. Pilas y Colas/Sis204_Pilas/pila.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sis204_Pilas
+{
+    class pila
+    {
+        public int maxpila = 3;
+        public int tope;
+        public int[] lista;
+
+        public pila()
+        {
+            tope = -1;
+            lista = new int[maxpila];
+        }
+
+        private bool vacia()
+        {
+            if (tope == -1)
+                return true;
+            else
+                return false;
+        }
+
+        private bool llena()
+        {
+            if (tope == maxpila - 1)
+                return true;
+            else
+                return false;
+        }
+
+        public void insertar(int x)
+        {
+            if (llena())
+                MessageBox.Show("La pila esta llena, no se puede insertar mas elementos");
+            else
+            {
+                tope++;
+                lista[tope] = x;
+            }
+        }
+        public void eliminar()
+        {
+            if(vacia())
+                MessageBox.Show("La pila esta vacia, no se puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("Elemento eliminado es: "+lista[tope]);
+                tope--; 
+            }
+        }
+    }
+}
