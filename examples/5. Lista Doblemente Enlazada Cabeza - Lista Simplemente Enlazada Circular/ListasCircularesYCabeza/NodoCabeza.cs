950874f3792661d409bdaf0fccc4398bfd9f23f8
Grover101 <limachigrover09@gmail.com>
2023-05-14 22:12:39 -0400

    add class node, double node and head node

---
+++ examples/5. Lista Doblemente Enlazada Cabeza - Lista Simplemente Enlazada Circular/ListasCircularesYCabeza/NodoCabeza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesYCabeza
+{
+    public class NodoCabeza
+    {
+        private int info;
+        private NodoDoble enlace;
+
+        public int Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+
+        public NodoDoble Enlace
+        {
+            get { return enlace; }
+            set { enlace = value; }
+        }
+    }
+}
---
+++ examples/5. Lista Doblemente Enlazada Cabeza - Lista Simplemente Enlazada Circular/ListasCircularesYCabeza/NodoDoble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesYCabeza
+{
+    public class NodoDoble
+    {
+        private char info;
+        private NodoDoble enlaceIzquierdo;
+        private NodoDoble enlaceDerecho;
+        public char Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+
+        public NodoDoble EnlaceIzquierdo
+        {
+            get { return enlaceIzquierdo; }
+            set { enlaceIzquierdo = value; }
+        }
+        public NodoDoble EnlaceDerecho
+        {
+            get { return enlaceDerecho; }
+            set { enlaceDerecho = value; }
+        }
+    }
+}
