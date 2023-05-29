e5dcc6b033639dad5ae7c67e87b62cffdc40f0ed
Grover101 <limachigrover09@gmail.com>
2023-05-29 02:43:25 -0400

    add class nodo and nodo hilvanado

---
+++ examples/8. Arboles Binarios II/ArbolesBinarios/Nodo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    class Nodo
+    {
+        private int info;
+        private Nodo enlaceIzquierdo;
+        private Nodo enlaceDerecho;
+
+        public int Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+        public Nodo EnlaceIzquierdo
+        {
+            get { return enlaceIzquierdo; }
+            set { enlaceIzquierdo = value;  }
+        }
+        public Nodo EnlaceDerecho
+        {
+            get { return enlaceDerecho; }
+            set { enlaceDerecho = value; }
+        }
+    }
+}
---
+++ examples/8. Arboles Binarios II/ArbolesBinarios/NodoHilvanado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    class NodoHilvanado
+    {
+        private int info;
+        private NodoHilvanado enlaceIzquierdo;
+        private NodoHilvanado enlaceDerecho;
+        //para realizar el hilvan del arbol, definimos los indicadores
+        private bool indicadorIzquierdo;
+        private bool indicadorDerecho;
+        public int Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+        public NodoHilvanado EnlaceIzquierdo
+        {
+            get { return enlaceIzquierdo; }
+            set { enlaceIzquierdo = value; }
+        }
+        public NodoHilvanado EnlaceDerecho
+        {
+            get { return enlaceDerecho; }
+            set { enlaceDerecho = value; }
+        }
+        public bool IndicadorIzquierdo
+        {
+            get { return indicadorIzquierdo; }
+            set { indicadorIzquierdo = value; }
+        }
+        public bool IndicadorDerecho
+        {
+            get { return indicadorDerecho; }
+            set { indicadorDerecho = value; }
+        }
+    }
+}
