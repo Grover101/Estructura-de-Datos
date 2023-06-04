4b72fc4a656e44162c19c1ea1456933533f03d99
Grover101 <limachigrover09@gmail.com>
2023-06-04 19:42:25 -0400

    add class arco and vertice

---
+++ examples/9. Grafos/Grafos/Arco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Arco
+    {
+        private Arco enlaceArco;
+        private Vertice enlaceVertice;
+
+        public Arco EnlaceArco
+        {
+            get { return enlaceArco; }
+            set { enlaceArco = value; }
+        }
+        public Vertice EnlaceVertice
+        {
+            get { return enlaceVertice; }
+            set { enlaceVertice = value; }
+        }
+    }
+}
---
+++ examples/9. Grafos/Grafos/Vertice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Vertice
+    {
+        private int info;
+        private Vertice enlaceIzquierdo;
+        private Vertice enlaceDerecho;
+        private Arco enlaceArco;
+
+        public int Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+
+        public Vertice EnlaceIzquierdo
+        {
+            get { return enlaceIzquierdo; }
+            set { enlaceIzquierdo = value; }
+        }
+        public Vertice EnlaceDerecho
+        {
+            get { return enlaceDerecho; }
+            set { enlaceDerecho = value; }
+        }
+        public Arco EnlaceArco
+        {
+            get { return enlaceArco; }
+            set { enlaceArco = value; }
+        }
+    }
+}
